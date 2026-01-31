c015e73552d77926a8ae7e55e460ded35d677436
Javier Arroliga <jarrolig@charlotte.edu>
2026-01-31 18:50:18 -0500

    Replace Slideshow with Calendar feature
    Renamed SlideshowFragment and related files to CalendarFragment, updated navigation, menu, and string resources to reflect the new Calendar feature. Integrated material-calendar-view library and updated the layout to use CalendarView.

--- app/src/main/java/com/example/medicationtracker/ui/calendar/CalendarViewModel.java
+++ app/src/main/java/com/example/medicationtracker/ui/calendar/CalendarViewModel.java
@@ -1,14 +1,14 @@
-package com.example.medicationtracker.ui.slideshow;
+package com.example.medicationtracker.ui.calendar;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class CalendarViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public SlideshowViewModel() {
+    public CalendarViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is slideshow fragment");
     }
